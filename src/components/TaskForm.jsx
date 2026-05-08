71d2a581c417def5edc998939b8a749492f7666c
Lubeth <lubethjimenez@yahoo.com>
2026-05-07 20:56:29 -0400

    Add handleSubmit function to controlled TaskForm

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -5,9 +5,13 @@ function TaskForm() {
       const [taskText, setTaskText] = useState("");
       const [taskPriority, setTaskPriority] = useState("Low");
 
+function handleSubmit(event){
+    event.preventDefault();
+}
+
   return (
     <div>
-       <form >
+       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Enter your task here..." value={taskText} onChange={(event)=> setTaskText(event.target.value)}/>
         <select value={taskPriority} onChange={(event)=> setTaskPriority(event.target.value)}>
             <option>Low</option>
